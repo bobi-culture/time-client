e0581364b66ad1de47648e93363d895592adc1d2
Babak <farhang.babak@gmail.com>
2022-07-19 12:50:32 +0430

    After Docker problem that I had with Connection for Client

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Time Client</h1>";
+    return "<h1>Time Client !!! </h1>";
 
 @app.route("/gettime")
 def get_time():
-    response = requests.get('http://localhost:5000/api/time')
+    response = requests.get('localhost:5000/api/time')
     json_response = response.json()
     data = json_response['time']
     return f"<font face=tahoma size=20 color=red>{data}</font>"
